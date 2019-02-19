337ed6e328b39198e12e99b347a33718f3550ba7
agtourcj3 <lucian.maniu@gmail.com>
2019-02-19 10:16:49 +0200

    Test pentru schimbare culoare produs selectat

---
+++ src/test/java/org/fasttrackit/ProductChangingColorTest.java
@@ -0,0 +1,40 @@
+package org.fasttrackit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.sleep;
+
+public class ProductChangingColorTest {
+
+    @Test
+    public void changeTheColorForItemFromSearchResultsTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        System.out.println("Opened homepage.");
+
+        String keyword = "vase";
+
+        driver.findElement(By.className("input-text")).sendKeys("vase" + Keys.ENTER);
+
+        //se poate face si cu link = http...link-ul imaginii, dar testul nu e stabil
+
+        driver.findElement(By.className("option-black")).click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String productName = "Modern Murray Ceramic Vase";
+        driver.findElement(By.xpath("//div[@class='product-info' and  .//a[text() ='" + productName + "'] and .//span[@class ='swatch-label']]//img[@alt = 'black']")).click();
+
+        String initialProductImage = driver.findElement(By.xpath("//div[@class='category-products']//a[@title ='Modern Murray Ceramic Vase']//img[@class!='hidden']")).getAttribute("src");
+//        assertThat()
+
+    }
+}
